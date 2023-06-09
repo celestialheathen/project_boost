a66f043ea6c7d1ceccdb0b061cf0e80633d27c8d
JingChao Yu <jingchaoyu@hotmail.com>
2023-06-09 19:33:09 -0400

    Tuned rocket ship movements, added serialize fields and collision

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 250;
+    [SerializeField] float mainThrust = 50f;
     Rigidbody rgbd;
     AudioSource audiosource;
 
@@ -15,25 +17,56 @@ public class Rocket : MonoBehaviour
 
     void Update()
     {
-       ProcessInput(); 
+       Thrust(); 
+       Rotate();
     }
 
-    private void ProcessInput()
+    private void OnCollisionEnter(Collision other) 
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            default: 
+                print("Dead");
+                break;
+        }
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rgbd.AddRelativeForce(Vector3.up);
-            audiosource.Play();
+            rgbd.AddRelativeForce(Vector3.up * mainThrust);
+
+            if (!audiosource.isPlaying)
+            {
+                audiosource.Play();
+            }
         }
-        
+        else 
+        {
+            audiosource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {   
+        rgbd.freezeRotation = true;
+
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationSpeed);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(Vector3.back * rotationSpeed);
         }
+
+        rgbd.freezeRotation = false;
 
     }
 }
